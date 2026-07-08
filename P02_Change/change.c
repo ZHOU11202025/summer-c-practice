c4272f5c751c46fcd702a7dc27824d67d6eea9e7
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-07-08 14:59:42 +0800

    add project P02

---
+++ P02_Change/change.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int price = 0;
+	printf("请输入商品价格（元）：\n");
+	scanf("%d", &price);
+	int change = 100 - price;
+	printf("还剩余：%d元",change);
+}
