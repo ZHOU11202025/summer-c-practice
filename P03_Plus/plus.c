c9f56ca410066d735533f5ef5f06296c9efd486a
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-07-08 17:36:57 +0800

    add Project P03

---
+++ P03_Plus/plus.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	printf("«Î ‰»Îa£¨bµƒ÷µ£∫\n");
+	scanf("%d %d",&a, &b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	return 0;
+}
