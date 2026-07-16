996af5154f7362485c03e954a95a34f9a5458bf7
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-07-16 14:18:27 +0800

    add P13

---
+++ Class_Practice/P13_GCD/GCD.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, t;
+	scanf("%d %d",&a,&b);
+	while (b != 0) {
+		t = a % b;
+		a = b;
+		b = t;
+	}
+	printf("%d\n",a);
+	return 0;
+}
